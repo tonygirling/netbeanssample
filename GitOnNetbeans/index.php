41fbf0fa4447d6976bb9b8f6dfe59f36f1f9d649
tony <tony@192.168.1.74>
2017-06-07 09:58:43 +0100

    Yahoo

---
+++ GitOnNetbeans/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<!--
+Author: Tony Girling
+Copyright 2017 Mornington Software Consultants: http://www.ms-consultants.co.uk
+Created 07-Jun-2017
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
